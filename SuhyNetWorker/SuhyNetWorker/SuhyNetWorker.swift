32c4362cf61ee0a3e3b18dda004b52f11d5bb3b9
苏鸿翊 <317112065@qq.com>
2021-02-05 14:34:24 +0800

    1.3.1

--- SuhyNetWorker/SuhyNetWorker/SuhyNetWorker.swift
+++ SuhyNetWorker/SuhyNetWorker/SuhyNetWorker.swift
@@ -8,6 +8,11 @@
 import Foundation
 import Alamofire
 
+open class SuhyNetWorkerBaseModel:NSObject,Codable {
+
+}
+
+public typealias modelClass = SuhyNetWorkerBaseModel
 /// api协议
 public protocol SuhyNetWorkerProtocol {
     var baseUrl:String! { get }
@@ -18,6 +23,21 @@ public protocol SuhyNetWorkerProtocol {
     var dynamicParams:[String: AnyObject]! { get }
     var isNeedCache:Bool! { get }
     var encoding:ParameterEncoding! { get }
+}
+  
+public protocol SuhyNetWorkerWithModelProtocol{
+    var mdoelType:modelClass.Type{ get }
+    var objKeyStr:String!{ get }
+}
+
+public protocol SuhyNetWorkerApiModelProtocol:SuhyNetWorkerProtocol,SuhyNetWorkerWithModelProtocol{
+    
+}
+
+public enum RequestModel {
+    case model(model:modelClass)
+    case List(ary:[modelClass])
+    case none
 }
 
 
@@ -30,4 +50,79 @@ public func requestAPIModel(api:SuhyNetWorkerProtocol,finishedCallback:@escaping
     SuhyNetWorker.request(api.url, method:api.apiType, params: api.params, dynamicParams: api.dynamicParams, encoding: api.encoding, headers: api.headParams).responseCacheAndJson { (obj) in
         finishedCallback(obj)
     }
+}
+
+/// aip访问，返回model和结果
+/// - Parameters:
+///   - api: api模型
+///   - finishedCallback: 返回结果，和转换好了的模型
+/// - Returns: 无
+public func requestAPIModel(api:SuhyNetWorkerApiModelProtocol,finishedCallback:@escaping (_ result : SuhyValue<Any>,_ data : RequestModel) -> ()){
+    SuhyNetWorker.requestApiwithReturnModel(someModel: api.mdoelType.self, api: api) { (result, lists) in
+        finishedCallback(result,lists)
+    }
+}
+
+/// api访问
+/// - Parameters:
+///   - api: api
+///   - someModel: 将要转换的模型类型，需要遵守Codable协议
+///   - finishedCallback: 返回模型（模型数组或者单个模型），返回是否成功的提示语句
+/// - Returns:
+fileprivate func requestApiwithReturnModel<T:modelClass>(someModel:T.Type,api:SuhyNetWorkerApiModelProtocol,finishedCallback:@escaping (_ result : SuhyValue<Any>,_ data : RequestModel) -> ())
+{
+    SuhyNetWorker.requestAPIModel(api: api) { (obj) in
+        
+        var requestDataModel:RequestModel = .none
+        switch obj.result{
+        case .success(let data):
+            if let resultdic = data  as? [String : AnyObject]
+            {
+                if let tempdic = resultdic[api.objKeyStr] as? [String : AnyObject]{
+                    let temp = SuhyNetWorker.toModel(someModel:someModel, dic: tempdic)
+                    requestDataModel = .model(model: temp)
+                }
+                else if let tempArray = resultdic[api.objKeyStr] as? [[String : AnyObject]]{
+                    var models = [T]()
+                    for dic in tempArray {
+                        let temp = SuhyNetWorker.toModel(someModel:someModel, dic: dic)
+                        models.append(temp)
+                    }
+                    requestDataModel = .List(ary: models)
+                }
+                else{
+                    requestDataModel = .none
+                }
+                finishedCallback(obj,requestDataModel)
+            }
+            break
+        case .failure(let error):
+            
+            finishedCallback(obj,requestDataModel)
+            
+            break
+        }
+    }
+
+}
+
+func DictionaryToJSON(dic:Any) -> Data {
+    if (!JSONSerialization.isValidJSONObject(dic as! NSDictionary)) {
+        print("无法解析出JSONString")
+        return Data()
+    }
+    return try! JSONSerialization.data(withJSONObject: dic, options: [])
+}
+
+func ArrayToJSON(array:[Any]) -> Data {
+    if (!JSONSerialization.isValidJSONObject(array as! NSArray)) {
+        print("无法解析出JSONString")
+        return Data()
+    }
+    return try! JSONSerialization.data(withJSONObject: array, options: [])
+}
+ func toModel<T:Codable>(someModel:T.Type,dic:Any)->T {
+    let json = SuhyNetWorker.DictionaryToJSON(dic:dic)
+    let temp = try! JSONDecoder().decode(someModel, from: json)
+    return temp
 }
